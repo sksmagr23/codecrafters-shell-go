3118efe8a9270f8142cbe26f409c1e5f8642a564
sksmagr23 <sakshamag34@gmail.com>
2024-12-19 21:45:12 +0530

    #FF0

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,14 +10,18 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = input[:len(input)-1]
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = input[:len(input)-1]
 
-	if input != "" {
-		fmt.Fprintf(os.Stderr, "%s: command not found\n", input)
+		if input == "exit" {
+			break
+		}
+		if input != "" {
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", input)
+		}
 	}
 }
