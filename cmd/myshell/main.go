f5d4421536250e5aa6f44082998641ce7eca0e06
sksmagr23 <sakshamag34@gmail.com>
2024-12-19 21:58:48 +0530

    #EZ5

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -17,11 +17,23 @@ func main() {
 		reader := bufio.NewReader(os.Stdin)
 		input, _ := reader.ReadString('\n')
 		input = input[:len(input)-1]
-		
+
 		if len(input) >= 5 && input[:5] == "echo " {
 			fmt.Println(input[5:])
 			continue
 		}
+		
+		if len(input) >= 5 && input[:5] == "type " {
+			command := input[5:]
+			switch command {
+			case "echo", "exit", "type":
+				fmt.Printf("%s is a shell builtin\n", command)
+			default:
+				fmt.Printf("%s: not found\n", command)
+			}
+			continue
+		}
+		
 		if input == "exit 0" {
 			break
 		}
