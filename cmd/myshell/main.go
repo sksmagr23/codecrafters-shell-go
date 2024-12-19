fedc7f5e5520f5ae66f8b3a41c96fd6824ed99ed
sksmagr23 <sakshamag34@gmail.com>
2024-12-19 22:26:01 +0530

    #MG5

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -22,39 +22,36 @@ func main() {
 			fmt.Println(input[5:])
 			continue
 		}
+		
 		if len(input) >= 5 && input[:5] == "type " {
 			command := input[5:]
 			switch command {
 			case "echo", "exit", "type":
 				fmt.Printf("%s is a shell builtin\n", command)
 			default:
-				fmt.Printf("%s: not found\n", command)
+				pathEnv := os.Getenv("PATH")
+				paths := strings.Split(pathEnv, ":")
+				flag := false
+				for _, path := range paths {
+					fullPath := path + "/" + command
+					if _, err := os.Stat(fullPath); err == nil {
+						fmt.Printf("%s is %s\n", command, fullPath)
+						flag = true
+						break
+					}
+				}
+				if !flag {
+					fmt.Printf("%s: not found\n", command)
+				}
 			}
 			continue
 		}
+
 		if input == "exit 0" {
 			break
 		}
 		if input != "" {
 			fmt.Fprintf(os.Stderr, "%s: command not found\n", input)
-		}
-		if len(input) >= 5 && input[:5] == "type " {
-			command := input[5:]
-			pathEnv := os.Getenv("PATH")
-			paths := strings.Split(pathEnv, ":")
-			flag := false
-			for _, path := range paths {
-				fullPath := path + "/" + command
-				if _, err := os.Stat(fullPath); err == nil {
-					fmt.Printf("%s is %s\n", command, fullPath)
-					flag = true
-					break
-				}
-			}
-			if !flag {
-				fmt.Printf("%s: not found\n", command)
-			}
-			continue
 		}
 
 	}
