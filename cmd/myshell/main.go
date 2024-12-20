4b7ae302c899ce58f9aea737f90e21a718184022
Saksham Agrawal <153260291+sksmagr23@users.noreply.github.com>
2024-12-20 14:00:31 +0530

    Update main.go

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,207 +6,169 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"unicode"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
-	dict := map[string]string{
-		"echo": "echo is a shell builtin",
-		"exit": "exit is a shell builtin",
-		"type": "type is a shell builtin",
-		"pwd":  "pwd is a shell builtin",
-	}
-	currentDir := ""
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
+
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if input == "exit 0" {
+			break
 		}
-		var cmd []string
-		var firstSpace int
-		var parsedInput = []string{}
-		if command[0] == '"' || command[0] == '\'' {
-			parsedInput = parseInput(command[firstSpace:])
-			cmd = append(cmd, parsedInput[0])
-			parsedInput = parsedInput[1:]
-		} else {
-			cmd = strings.Split(command[:len(command)-1], " ")
-			firstSpace = strings.IndexRune(command, ' ')
-			if firstSpace != -1 {
-				parsedInput = parseInput(command[firstSpace:])
-			}
+
+		if len(input) >= 5 && input[:5] == "echo " {
+			words := parseArguments(input[5:])
+			fmt.Println(strings.Join(words, " "))
+			continue
 		}
-		switch cmd[0] {
-		case "exit":
-			os.Exit(0)
-		case "echo":
-			fmt.Println(strings.Join(parsedInput, " "))
-		case "type":
-			val, ok := dict[cmd[1]]
-			if ok {
-				fmt.Println(val)
-			} else {
-				var found bool
-				env := os.Getenv("PATH")
-				paths := strings.Split(env, ":")
-				for _, path := range paths {
-					exec := path + "/" + cmd[1]
-					if _, err := os.Stat(exec); err == nil {
-						fmt.Fprintf(os.Stdout, "%v is %v\n", cmd[1], exec)
-						found = true
-						break
-					}
+
+		if len(input) >= 4 && input[:4] == "cat " {
+			files := parseArguments(input[4:])
+			for _, file := range files {
+				content, err := os.ReadFile(file)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "cat: %s: No such file or directory\n", file)
+					continue
 				}
-				if !found {
-					fmt.Println(cmd[1] + ": not found")
+				fmt.Print(string(content))
+			}
+			continue
+		}
+
+		if len(input) >= 5 && input[:5] == "type " {
+			command := input[5:]
+			switch command {
+			case "echo", "exit", "type", "pwd", "cd":
+				fmt.Printf("%s is a shell builtin\n", command)
+			default:
+				path := findExecutable(command)
+				if path != "" {
+					fmt.Printf("%s is %s\n", command, path)
+				} else {
+					fmt.Printf("%s: not found\n", command)
 				}
 			}
-		case "pwd":
-			if currentDir == "" {
-				pwd, _ := os.Getwd()
-				fmt.Println(pwd)
-			} else {
-				fmt.Println(currentDir)
-			}
-		case "cd":
-			if cmd[1][0] == '/' {
-				_, err := os.Stat(cmd[1])
-				if os.IsNotExist(err) {
-					fmt.Printf("cd: %s: No such file or directory\n", cmd[1])
-				} else {
-					currentDir = cmd[1]
-				}
-			} else if cmd[1][0] == '.' && cmd[1][1] == '/' {
-				_, err := os.Stat(currentDir + cmd[1][1:])
-				if os.IsNotExist(err) {
-					fmt.Printf("cd: %s: No such file or directory\n", cmd[1])
-				} else {
-					currentDir = currentDir + cmd[1][1:]
-				}
-			} else if cmd[1][0] == '~' {
-				currentDir = os.Getenv("HOME")
-			} else {
-				var counter int = 0
-				var i int = 0
-				for i = 0; i <= len(string(cmd[1]))-3; i = i + 3 {
-					if cmd[1][i:i+3] == "../" {
-						counter++
-					} else {
-						return
-					}
-				}
-				currentDirSplit := strings.Split(currentDir[1:], "/")
-				var tryDir string
-				if i == len(cmd[1]) {
-					tryDir = "/" + strings.Join(currentDirSplit[:len(currentDirSplit)-counter], "/")
-				} else {
-					tryDir = "/" + strings.Join(currentDirSplit[:len(currentDirSplit)-counter], "/") + string(cmd[1][i:])
-				}
-				_, err := os.Stat(tryDir)
-				if os.IsNotExist(err) {
-					fmt.Printf("cd: %s: No such file or directory\n", tryDir)
-				} else {
-					currentDir = tryDir
-				}
-			}
-		default:
-			command2 := exec.Command(cmd[0], parsedInput...)
-			command2.Stderr = os.Stderr
-			command2.Stdout = os.Stdout
-			err := command2.Run()
+			continue
+		}
+
+		if input == "pwd" {
+			dir, err := os.Getwd()
 			if err != nil {
-				fmt.Println(command[:len(command)-1] + ": command not found")
+				fmt.Fprintf(os.Stderr, "Error getting current directory: %v\n", err)
+			} else {
+				fmt.Println(dir)
 			}
+			continue
+		}
+
+		if len(input) >= 3 && input[:3] == "cd " {
+			dir := strings.TrimSpace(input[3:])
+			if dir == "~" {
+				dir = os.Getenv("HOME")
+			}
+			err := os.Chdir(dir)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "cd: %s: No such file or directory\n", dir)
+			}
+			continue
+		}
+
+		args := parseArguments(input)
+		if len(args) == 0 {
+			continue
+		}
+
+		executable := unescapeAndTrimQuotes(args[0])
+
+		path, err := exec.LookPath(executable)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", args[0])
+			continue
+		}
+
+		for i, arg := range args {
+			args[i] = unescapeAndTrimQuotes(arg)
+		}
+
+		cmd := exec.Command(path, args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error running command: %v\n", err)
 		}
 	}
 }
-func parseInput(s string) (args []string) {
-	s = strings.TrimSpace(s)
-	var quote string
-	var arg string
-	var backSlashed bool
-	var insideSingleQuote bool
-	for i, val := range s {
-		if (string(val) == "'" || string(val) == "\"") && len(quote) == 0 {
-			if backSlashed {
-				arg = arg + string(val)
-				backSlashed = false
-				continue
-			}
-			quote = string(val)
+
+
+func unescapeAndTrimQuotes(input string) string {
+    if len(input) > 0 && (input[0] == '\'' || input[0] == '"') {
+        input = strings.Trim(input, "'\"")
+    }
+    input = strings.ReplaceAll(input, "\\'", "'")
+    input = strings.ReplaceAll(input, "\\\"", "\"")
+    return input
+}
+
+
+
+func findExecutable(command string) string {
+	pathEnv := os.Getenv("PATH")
+	paths := strings.Split(pathEnv, ":")
+	for _, path := range paths {
+		fullPath := path + "/" + command
+		if _, err := os.Stat(fullPath); err == nil {
+			return fullPath
+		}
+	}
+	return ""
+}
+
+func parseArguments(input string) []string {
+	var args []string
+	var currentArg strings.Builder
+	var inQuotes bool
+	var quoteChar rune
+	var escapeNext bool
+
+	for _, char := range input {
+		if escapeNext {
+			currentArg.WriteRune(char)
+			escapeNext = false
 			continue
 		}
-		if string(val) == quote {
-			if backSlashed {
-				arg = arg + string(val)
-				backSlashed = false
-				continue
+
+		switch char {
+		case '\\':
+			escapeNext = true
+		case ' ', '\t':
+			if inQuotes {
+				currentArg.WriteRune(char)
+			} else if currentArg.Len() > 0 {
+				args = append(args, currentArg.String())
+				currentArg.Reset()
 			}
-			quote = ""
-			continue
-		}
-		if quote == "\"" && string(val) == "'" {
-			if insideSingleQuote {
-				insideSingleQuote = false
+		case '\'', '"':
+			if inQuotes && char == quoteChar {
+				inQuotes = false
+			} else if !inQuotes {
+				inQuotes = true
+				quoteChar = char
 			} else {
-				insideSingleQuote = true
+				currentArg.WriteRune(char)
 			}
-			arg = arg + string(val)
-			continue
+		default:
+			currentArg.WriteRune(char)
 		}
-		if unicode.IsSpace(val) {
-			if len(quote) == 0 {
-				if len(arg) == 0 {
-					continue
-				}
-				if len(arg) > 0 {
-					if backSlashed {
-						arg = arg + string(val)
-						backSlashed = false
-						continue
-					}
-					args = append(args, arg)
-					arg = ""
-					continue
-				}
-			}
-			if len(quote) == 1 {
-				arg = arg + string(val)
-				continue
-			}
-		}
-		if val == '\\' {
-			if len(quote) == 0 {
-				backSlashed = true
-				continue
-			}
-			if quote == "\"" {
-				if insideSingleQuote || backSlashed {
-					arg = arg + string(val)
-					backSlashed = false
-					continue
-				}
-				if i <= len(s)-2 {
-					if string(s[i+1]) == "\"" || string(s[i+1]) == "$" || string(s[i+1]) == "\\" {
-						backSlashed = true
-						continue
-					}
-					arg = arg + string(val)
-					backSlashed = false
-					continue
-				}
-			}
-		}
-		arg = arg + string(val)
-		backSlashed = false
 	}
-	if len(arg) > 0 {
-		args = append(args, arg)
+
+	if currentArg.Len() > 0 {
+		args = append(args, currentArg.String())
 	}
-	return
+
+	return args
 }
